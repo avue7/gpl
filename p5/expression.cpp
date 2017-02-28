a2905e0aba646e5066fbd8ef706200e31767172a
avue7 <avue7@mail.csuchico.edu>
2017-02-27 23:23:57 -0800

    Dont want to lose what I typed so far. Got the constructors for const and unary and binary done. Not sure what to do next. I'm looking at the eval_ints and such next I guess.

--- p5/expression.cpp
+++ p5/expression.cpp
@@ -1,9 +1,67 @@
 #include "expression.h"
 
+// unary and binary operator
 Expression::Expression(Operator_type operator_type, Gpl_type gpl_type, Expression *lhs, Expression *rhs)
 {
-  m_operator_type = operator_type;
   m_gpl_type = gpl_type;
   m_lhs = lhs;
   m_rhs = rhs;
+  // Binary Operators
+  if (operator_type == PLUS)
+    m_oper = operator_type;
+    m_node = BINARY_OPERATOR;
+  else if (operator_type == MINUS)
+    m_oper = operator_type;
+    m_node = BINARY_OPERATOR;
+  else if (operator_type == MULTIPLY)
+    m_oper = operator_type;
+    m_node = BINARY_OPERATOR;
+  else if (operator_type == DIVIDE)
+    m_oper = operator_type;
+    m_node = BINARY_OPERATOR;
+  else if (operator_type == MOD)
+    m_oper = operator_type;
+    m_node = BINARY_OPERATOR;
+  // Unary Operators
+  else if (operator_type == UNARY_MINUS)
+    m_oper = operator_type;
+    m_node = UNARY_OPERATOR;
+  else if (operator_type == SIN)
+    m_oper = operator_type;
+    m_node = UNARY_OPERATOR;
+  else if (operator_type == COS)
+    m_oper = operator_type;
+    m_node = UNARY_OPERATOR;
+  else if (operator_type == TAN)
+    m_oper = operator_type;
+    m_node = UNARY_OPERATOR;
+  else if (operator_type == ASIN)
+    m_oper = operator_type;
+    m_node = UNARY_OPERATOR;
+  else if (operator_type == ACOS)
+    m_oper = operator_type;
+    m_node = UNARY_OPERATOR;
+  else if (operator_type == ATAN)
+    m_oper = operator_type;
+    m_node = UNARY_OPERATOR;
+  else if (operator_type == RANDOM)
+    m_oper = operator_type;
+    m_node = UNARY_OPERATOR;
+  else if (operator_type == SQRT)
+    m_oper = operator_type;
+    m_node = UNARY_OPERATOR;
+  else
+    cerr << "Error: cannot get operator type in constructor." << endl;
 }
+
+// Constructor for constants
+Expression::Expression(void *value, Gpl_type gpl_type, Expression *lhs, Expression *rhs)
+{
+  m_value = value;
+  m_gpl_type = gpl_type;
+  m_lhs = lhs;
+  m_rhs = rhs;
+  m_node = CONSTANT // Set this node_type to CONSTANT
+}
+
+
--- p5/expression.h
+++ p5/expression.h
@@ -15,24 +15,27 @@ class Expression
     // Member variables declared here
     Expression *m_lhs;
     Expression *m_rhs;
-    Operator_type m_operator_type;
+    Operator_type m_oper;
     Gpl_type m_gpl_type;
     void *m_value;
-    Node_type m_node_type;
+    Node_type m_node; // To store the type of Nodes
 
-    int eval_int();
-    double eval_double();
-    string eval_string();
+    int eval_int();         // To return evaluated ints
+    double eval_double();   // to return evaluated doubles
+    string eval_string();   // To return evaluated strings
     
+
+    // Member functions: 
+
     // Constructor for unary and binary operator
     Expression (Operator_type operator_type, Gpl_type gpl_type, Expression *lhs, Expression *rhs);
     // Constructor for constant
-    Expression (void *m_value, Gpl_type gpl_type, Expression *m_lhs, Expression *m_rhs);
+    Expression (void *value, Gpl_type gpl_type, Expression *lhs, Expression *rhs);
    
    
   private:
-    enum Tree_kinds {EXPRESSION, CONSTANT, VARIABLE};
+    enum Tree_kinds {EXPRESSION, CONSTANT, VARIABLE}; // since im making a node maybe i dont need this
     Tree_kinds m_kind;
-    Expression *evaluation();
+    Expression *evaluation(); // member function to build tree; recursively evaluates each node
 };
 #endif
